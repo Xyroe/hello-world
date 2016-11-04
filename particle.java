9ed3fdfe36c62a479ffdd368b7e9e46b506d9690
Xyroe <Masteralexte@gmail.com>
2016-11-04 01:42:17 -0400

    Update particle.java

--- particle.java
+++ particle.java
@@ -1,30 +1,21 @@
-
 public class particle {
 
 	private String identity;
 	private double mass;
-	private double momentum;
-	private double acceleration;
-	private double velocity;
-	private double[] position;
+	private double[] momentum = {0,0,0};
+	private double[] acceleration = {0,0,0};
+	private double[] velocity = {0,0,0};
+	private double[] position = {0,0,0};
 	
 	public particle(){
 		identity = "";
 		mass = 0;
-		momentum = 0;
-		acceleration = 0;
-		velocity = 0;
-		position;
 		
 	}
 	
-	public particle(String a, double b, double c, double d, double e, double[] f){
+	public particle(String a, double b){
 		identity = a;
 		mass = b;
-		momentum = c;
-		acceleration = d;
-		velocity = e;
-		position = f;
 		
 	}
 	
@@ -37,19 +28,78 @@ public class particle {
 	}
 	
 	public double getMomentum(){
-		return momentum;
+		return Math.sqrt(Math.pow(momentum[0],2)+Math.pow(momentum[1],2)+Math.pow(momentum[2],2));
 	}
 	
 	public double getAcceleration(){
-		return acceleration;
+		return Math.sqrt(Math.pow(acceleration[0],2)+Math.pow(acceleration[1],2)+Math.pow(acceleration[2],2));
 	}
 	
 	public double getVelocity(){
-		return velocity;
+		return Math.sqrt(Math.pow(velocity[0],2)+Math.pow(velocity[1],2)+Math.pow(velocity[2],2));
 	}
 	
-	public 
+	public String getPosition(){
+		String p = "";
+		for(int i = 0;i<position.length;i++){
+			p+=(position[i]+" ");
+		}
+		return p;
+	}
 	
+	public double getOriginDistance(){
+		return Math.sqrt(Math.pow(position[0],2)+Math.pow(position[1],2)+Math.pow(position[2],2));
+	}
 	
+	public double getPositionX(){
+		return position[0];
+	}
 	
+	public double getPositionY(){
+		return position[1];
+	}
+	
+	public double getPositionZ(){
+		return position[2];
+	}
+
+
+	public double getVelocityX(){
+		return velocity[0];
+	}
+	
+	public double getVelocityY(){
+		return velocity[1];
+	}
+	
+	public double getVelocityZ(){
+		return velocity[2];
+	}
+
+
+	public double getAccelerationX(){
+		return acceleration[0];
+	}
+	
+	public double getAccelerationY(){
+		return acceleration[1];
+	}
+	
+	public double getAccelerationZ(){
+		return acceleration[2];
+	}
+
+
+	public double getMomentumX(){
+		return momentum[0];
+	}
+	
+	public double getMomentumY(){
+		return momentum[1];
+	}
+	
+	public double getMomentumZ(){
+		return momentum[2];
+	}
+
 }
